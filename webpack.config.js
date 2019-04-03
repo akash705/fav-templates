dcaad59d748295265b8c6b862f4f6e51ab84a8db
Akash mishra <mishraakash273694@gmail.com>
2019-04-03 16:46:13 +0530

    mode set

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,44 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-  entry:{
-     app: './src/index.js',
-    //  multiple entry points
-    //  print: './src/second.js'
+module.exports = (env={mode:mode='production'})=>{
+  env.mode = env.mode || 'development'
+  return{
+    mode:env.mode,
+    entry:{
+      app: './src/index.js',
+     //  multiple entry points
+     //  print: './src/second.js'
+    },
+   output: {
+     filename: '[name].main.js',
+     path: path.resolve(__dirname, 'dist')
    },
-  output: {
-    filename: '[name].main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'inline-source-map',
-  plugins:[
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename:'index.html',
-      template:'./index.html',
-      title:'MyApp'
-    }),
-  ],
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    },{
-      test :/\.(png|svg|jpg|gif)$/,
-      use:[
-        'file-loader'
-      ]
-    },{
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }]
+   devtool: 'inline-source-map',
+   plugins:[
+     new CleanWebpackPlugin(),
+     new HtmlWebpackPlugin({
+       filename:'index.html',
+       template:'./index.html',
+       title:'MyApp'
+     }),
+   ],
+   module: {
+     rules: [{
+       test: /\.css$/,
+       use: [
+         'style-loader',
+         'css-loader'
+       ]
+     },{
+       test :/\.(png|svg|jpg|gif)$/,
+       use:[
+         'file-loader'
+       ]
+     },{
+       test: /\.scss$/,
+       use: ['style-loader', 'css-loader', 'sass-loader']
+     }]
+   }
   }
 };
